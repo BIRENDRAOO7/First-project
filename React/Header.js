697a774eccfaa09b406d3df9c52e803f612ab19d
birendra.rana <birendra.rana@essentia.dev>
2024-08-20 23:36:05 +0530

    Build a React info site | Organize Components not working

---
+++ React/Header.js
@@ -0,0 +1,14 @@
+export default function Header(){
+    return(
+        <header className="header">
+            <nav className="nav">
+                <img src = "https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png" className="nav-logo"/>
+                <ul className="nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
